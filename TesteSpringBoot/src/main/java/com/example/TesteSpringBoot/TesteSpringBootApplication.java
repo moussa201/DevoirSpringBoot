19dd5f84e8d7352b91a79c0806f2ca148b7ab3bb
moussa201 <38951287+moussa201@users.noreply.github.com>
2023-01-31 09:49:54 +0100

    first commit
    Creation du microservice 1

---
+++ TesteSpringBoot/src/main/java/com/example/TesteSpringBoot/TesteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TesteSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringBootApplication.class, args);
+	}
+
+}
